fa90924124186974f8dd376766103a9005402919
Math_RPG <maugusilva.santos@gmail.com>
2023-05-31 20:59:18 -0300

    Reformat motor/policy/loaders
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/ispd/policy/loaders/GridSchedulingPolicyLoader.java
+++ src/ispd/policy/loaders/GridSchedulingPolicyLoader.java
@@ -2,13 +2,12 @@ package ispd.policy.loaders;
 
 import ispd.policy.scheduling.grid.GridSchedulingPolicy;
 
-public class GridSchedulingPolicyLoader
-        extends GenericPolicyLoader<GridSchedulingPolicy> {
-    private static final String CLASS_PATH =
-            "ispd.policy.scheduling.grid.impl.";
+public class GridSchedulingPolicyLoader extends GenericPolicyLoader<GridSchedulingPolicy> {
+
+    private static final String CLASS_PATH = "ispd.policy.scheduling.grid.impl.";
 
     @Override
-    protected String getClassPath() {
+    protected String getClassPath () {
         return GridSchedulingPolicyLoader.CLASS_PATH;
     }
 }
