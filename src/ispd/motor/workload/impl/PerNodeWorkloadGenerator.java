19d0b37dfa267ea4339234e5ecafef8b94b8803d
Math_RPG <maugusilva.santos@gmail.com>
2023-05-31 20:47:08 -0300

    Reformat motor/workload/impl/
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/ispd/motor/workload/impl/PerNodeWorkloadGenerator.java
+++ src/ispd/motor/workload/impl/PerNodeWorkloadGenerator.java
@@ -1,5 +1,12 @@
 package ispd.motor.workload.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.function.IntSupplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import ispd.motor.filas.RedeDeFilas;
 import ispd.motor.filas.Tarefa;
 import ispd.motor.filas.servidores.CS_Processamento;
@@ -7,13 +14,6 @@ import ispd.motor.filas.servidores.CentroServico;
 import ispd.motor.random.Distribution;
 import ispd.motor.random.TwoStageUniform;
 import ispd.motor.workload.WorkloadGeneratorType;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
-import java.util.function.IntSupplier;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * Generates a workload for a single master node in a {@link RedeDeFilas}.<br>
@@ -33,37 +33,46 @@ import java.util.stream.IntStream;
  * @see Distribution#nextExponential(double)
  */
 public class PerNodeWorkloadGenerator extends RandomicWorkloadGenerator {
+
     private static final String USER_NO_DELAY = "NoDelay";
-    private static final int ON_NO_DELAY = 120;
-    private final String application;
-    private final String owner;
-    private final String schedulerId;
+    private static final int    ON_NO_DELAY   = 120;
+    private final        String application;
+    private final        String owner;
+    private final        String schedulerId;
 
     /**
      * Create a per-node workload generator with the given parameters.
      *
-     * @param application   workload's originating application
-     * @param owner         id (name) of the user who owns the workload
-     * @param schedulerId   id of the machine which the workload will be
-     *                      directed to
-     * @param taskCount     total number of tasks
-     * @param computation   {@link TwoStageUniform} for the computation size
-     * @param communication {@link TwoStageUniform} for the communication size
-     * @param idSupplier    supplier of ids for the generated tasks. Must
-     *                      supply at least {@code taskCount} ids
+     * @param application
+     *         workload's originating application
+     * @param owner
+     *         id (name) of the user who owns the workload
+     * @param schedulerId
+     *         id of the machine which the workload will be
+     *         directed to
+     * @param taskCount
+     *         total number of tasks
+     * @param computation
+     *         {@link TwoStageUniform} for the computation size
+     * @param communication
+     *         {@link TwoStageUniform} for the communication size
+     * @param idSupplier
+     *         supplier of ids for the generated tasks. Must
+     *         supply at least {@code taskCount} ids
      */
-    public PerNodeWorkloadGenerator(
+    public PerNodeWorkloadGenerator (
             final String application, final String owner,
             final String schedulerId, final int taskCount,
             final TwoStageUniform computation,
             final TwoStageUniform communication,
-            final IntSupplier idSupplier) {
+            final IntSupplier idSupplier
+    ) {
         super(
                 taskCount, computation, communication,
                 idSupplier, new Distribution(System.currentTimeMillis())
         );
         this.application = application;
-        this.owner = owner;
+        this.owner       = owner;
         this.schedulerId = schedulerId;
     }
 
@@ -73,28 +82,33 @@ public class PerNodeWorkloadGenerator extends RandomicWorkloadGenerator {
      * this method, and its {@link PerNodeWorkloadGenerator#toVector()
      * toVector} method.
      *
-     * @param row        {@link List} of attributes to construct a generator.
-     * @param idSupplier supplier of ids for the generated tasks.
+     * @param row
+     *         {@link List} of attributes to construct a generator.
+     * @param idSupplier
+     *         supplier of ids for the generated tasks.
+     *
      * @return an instance created from the given {@link List} of attributes.
      */
-    public static PerNodeWorkloadGenerator fromTableRow(
-            final List row, final IntSupplier idSupplier) {
+    public static PerNodeWorkloadGenerator fromTableRow (
+            final List row, final IntSupplier idSupplier
+    ) {
         class ListWrapper {
+
             private final List list;
 
-            private ListWrapper(final List list) {
+            private ListWrapper (final List list) {
                 this.list = list;
             }
 
-            private int getAsInt(final int i) {
+            private int getAsInt (final int i) {
                 return Integer.parseInt(this.get(i));
             }
 
-            private String get(final int i) {
+            private String get (final int i) {
                 return this.list.get(i).toString();
             }
 
-            private double getAsDouble(final int i) {
+            private double getAsDouble (final int i) {
                 return Double.parseDouble(this.get(i));
             }
         }
@@ -133,9 +147,10 @@ public class PerNodeWorkloadGenerator extends RandomicWorkloadGenerator {
      * represents instances of this class in a table.
      *
      * @return {@link Vector} with this instance's data.
+     *
      * @see TwoStageUniform
      */
-    public Vector<String> toVector() {
+    public Vector<String> toVector () {
         final var temp = new Vector<String>(8);
         temp.add(0, this.application);
         temp.add(1, this.owner);
@@ -153,22 +168,24 @@ public class PerNodeWorkloadGenerator extends RandomicWorkloadGenerator {
      * with id matching the one in {@link #schedulerId}. If no appropriate
      * master is found, this method returns an empty {@link ArrayList}.
      *
-     * @param qn {@link RedeDeFilas} with the master that will host the
-     *           {@link Tarefa}s.
+     * @param qn
+     *         {@link RedeDeFilas} with the master that will host the
+     *         {@link Tarefa}s.
+     *
      * @return {@link List} of {@link Tarefa}s, configured for the correct
-     * master, or an empty {@link ArrayList}, if the {@link RedeDeFilas} has
-     * no master with the correct id.
+     *         master, or an empty {@link ArrayList}, if the {@link RedeDeFilas} has
+     *         no master with the correct id.
      */
     @Override
-    public List<Tarefa> makeTaskList(final RedeDeFilas qn) {
+    public List<Tarefa> makeTaskList (final RedeDeFilas qn) {
         return qn.getMestres().stream()
-                .filter(this::hasCorrectId)
-                .findFirst()
-                .map(this::makeTaskListOriginatingAt)
-                .orElseGet(ArrayList::new);
+                 .filter(this::hasCorrectId)
+                 .findFirst()
+                 .map(this::makeTaskListOriginatingAt)
+                 .orElseGet(ArrayList::new);
     }
 
-    private boolean hasCorrectId(final CentroServico m) {
+    private boolean hasCorrectId (final CentroServico m) {
         return m.getId().equals(this.schedulerId);
     }
 
@@ -176,20 +193,22 @@ public class PerNodeWorkloadGenerator extends RandomicWorkloadGenerator {
      * Make a task list of appropriate size, with all tasks originating at
      * the given {@link CS_Processamento}.
      *
-     * @param origin that hosts all generated tasks.
+     * @param origin
+     *         that hosts all generated tasks.
+     *
      * @return {@link List} with generated {@link Tarefa}s.
      */
-    private List<Tarefa> makeTaskListOriginatingAt(final CS_Processamento origin) {
+    private List<Tarefa> makeTaskListOriginatingAt (final CS_Processamento origin) {
         return IntStream.range(0, this.taskCount)
-                .mapToObj(i -> this.makeTaskFor(origin))
-                .collect(Collectors.toList());
+                        .mapToObj(i -> this.makeTaskFor(origin))
+                        .collect(Collectors.toList());
     }
 
     /**
      * @return {@link WorkloadGeneratorType#PER_NODE}
      */
     @Override
-    public WorkloadGeneratorType getType() {
+    public WorkloadGeneratorType getType () {
         return WorkloadGeneratorType.PER_NODE;
     }
 
@@ -207,11 +226,11 @@ public class PerNodeWorkloadGenerator extends RandomicWorkloadGenerator {
      * @see TwoStageUniform
      */
     @Override
-    public String formatForIconicModel() {
+    public String formatForIconicModel () {
         return String.format("%s %d %f %f %f %f",
-                this.schedulerId, this.taskCount,
-                this.computation.maximum(), this.computation.minimum(),
-                this.communication.maximum(), this.communication.minimum()
+                             this.schedulerId, this.taskCount,
+                             this.computation.maximum(), this.computation.minimum(),
+                             this.communication.maximum(), this.communication.minimum()
         );
     }
 
@@ -221,16 +240,16 @@ public class PerNodeWorkloadGenerator extends RandomicWorkloadGenerator {
      * generating computation and communication sizes.
      */
     @Override
-    public String toString() {
+    public String toString () {
         return """
-                PerNodeWorkloadGenerator{
-                    taskCount=%d,
-                    application='%s',
-                    owner='%s',
-                    scheduler='%s',
-                    computation=%s,
-                    communication=%s,
-                }""".formatted(
+               PerNodeWorkloadGenerator{
+                   taskCount=%d,
+                   application='%s',
+                   owner='%s',
+                   scheduler='%s',
+                   computation=%s,
+                   communication=%s,
+               }""".formatted(
                 this.taskCount,
                 this.application,
                 this.owner,
@@ -242,33 +261,33 @@ public class PerNodeWorkloadGenerator extends RandomicWorkloadGenerator {
 
     /**
      * @return id of the scheduler (i.e., master) that will host the tasks
-     * generated by this
+     *         generated by this
      */
-    public String getSchedulerId() {
+    public String getSchedulerId () {
         return this.schedulerId;
     }
 
     /**
      * @return that which will be set as the tasks' originating application
      */
-    public String getApplication() {
+    public String getApplication () {
         return this.application;
     }
 
     /**
      * @return the user's id who is going to be set as the tasks' owner
      */
-    public String getOwner() {
+    public String getOwner () {
         return this.owner;
     }
 
     @Override
-    protected String makeTaskUser(final CS_Processamento master) {
+    protected String makeTaskUser (final CS_Processamento master) {
         return this.owner;
     }
 
     @Override
-    protected String makeTaskApplication() {
+    protected String makeTaskApplication () {
         return this.application;
     }
 
@@ -278,20 +297,21 @@ public class PerNodeWorkloadGenerator extends RandomicWorkloadGenerator {
      * depending on the task owner's id.
      *
      * @return task creation time (in seconds), to be used to make a new task.
+     *
      * @see #calculateExtraDelay()
      */
     @Override
-    protected double makeTaskCreationTime() {
+    protected double makeTaskCreationTime () {
         return this.random.nextExponential(5) + this.calculateExtraDelay();
     }
 
     /**
      * @return no extra delay for most users, but an extra
-     * {@value #ON_NO_DELAY} seconds if the task owner's id is
-     * {@value #USER_NO_DELAY}.
+     *         {@value #ON_NO_DELAY} seconds if the task owner's id is
+     *         {@value #USER_NO_DELAY}.
      */
-    private int calculateExtraDelay() {
+    private int calculateExtraDelay () {
         return PerNodeWorkloadGenerator.USER_NO_DELAY.equals(this.owner) ?
-                PerNodeWorkloadGenerator.ON_NO_DELAY : 0;
+               PerNodeWorkloadGenerator.ON_NO_DELAY : 0;
     }
 }
