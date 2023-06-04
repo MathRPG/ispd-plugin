627554c6a423c35a99e5e2fc0eee7cd3070fc304
Math_RPG <maugusilva.santos@gmail.com>
2023-06-04 14:13:25 -0300

    Reformat some files
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/ispd/annotations/Policy.java
+++ src/ispd/annotations/Policy.java
@@ -4,4 +4,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.SOURCE)
-public @interface Policy {}
+public @interface Policy {
+
+}
