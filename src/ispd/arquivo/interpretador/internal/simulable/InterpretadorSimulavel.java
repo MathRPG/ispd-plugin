bf1f0ccf78a778b9f5346471af8ebfac6d06f4e3
Math_RPG <maugusilva.santos@gmail.com>
2023-06-13 13:08:25 -0300

    Some work removing dead code from motor/queueNetworks, and associated callers (Intepretador mostly)
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/ispd/arquivo/interpretador/internal/simulable/InterpretadorSimulavel.java
+++ src/ispd/arquivo/interpretador/internal/simulable/InterpretadorSimulavel.java
@@ -1,60 +1,39 @@
 package ispd.arquivo.interpretador.internal.simulable;
 
 import java.io.File;
-import java.util.List;
-
+import java.io.FileInputStream;
 import javax.swing.JOptionPane;
-
-import ispd.motor.queueNetworks.RedesDeFilas;
-
 
 public class InterpretadorSimulavel {
 
-    Interpretador parser;
+    private Interpretador parser = null;
 
-    public InterpretadorSimulavel () {
-
-    }
-
-    public boolean leArquivo (File arquivo) {
-
+    public boolean leArquivo (final File arquivo) {
         try {
             try {
-                parser = new Interpretador(new java.io.FileInputStream(arquivo));
-                parser.verbose = false;
-                parser.printv("Modo verbose ligado");
-                parser.Modelo();
-            } catch (ParseException e) {
-                parser.erroEncontrado = true;
-                JOptionPane.showOptionDialog(null, "Foram encontrados os seguintes erros:\n" + e.getMessage(),
-                                             "Erros Encontrados", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
-                                             null, null, null
+                this.parser = new Interpretador(new FileInputStream(arquivo));
+                this.parser.setVerbose(false);
+                this.parser.printv("Modo verbose ligado");
+                this.parser.Modelo();
+            } catch (final ParseException e) {
+                this.parser.setErroEncontrado(true);
+                JOptionPane.showOptionDialog(
+                    null,
+                    "Foram encontrados os seguintes erros:\n" + e.getMessage(),
+                    "Erros Encontrados",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    null,
+                    null
                 );
-                return parser.erroEncontrado;
+                return this.parser.isErroEncontrado();
             }
-        } catch (Exception e) {
+        } catch (final Exception e) {
             e.printStackTrace();
-            return parser.erroEncontrado;
+            return this.parser.isErroEncontrado();
         }
 
-        return parser.erroEncontrado;
+        return this.parser.isErroEncontrado();
     }
-
-
-    public void escreveArquivo () {
-        //parser.escreveArquivo();
-    }
-
-    public RedesDeFilas getRedeFilas () {
-        return parser.getRedeFilas();
-    }
-
-    public String getTarefas () {
-        return parser.getTarefas();
-    }
-
-    public List<String> getNomeId () {
-        return parser.getNomeId();
-    }
-
 }
--- src/ispd/motor/queueNetworks/CentrosDeServico.java
+++ src/ispd/motor/queueNetworks/CentrosDeServico.java
@@ -1,28 +1,42 @@
 package ispd.motor.queueNetworks;
 
+import java.util.Collection;
 import java.util.HashSet;
 
 public class CentrosDeServico {
 
-    private final HashSet<Object> servidores;
-    private final HashSet<Object> filas;
-    private final int             numMaxServidores;  //Capacidade maxima de servidores que
-    private final int             idCS;
-    // podem ser adicionados ao CS.
-    //Esse numero considera apenas servidores de processamento (mestre e
-    // escravos)
-    //ou seja, se esse numero for 100, o servidor tera 101 servidores, sendo:
-    //1 servidor de comunicacao, 1 mestre e 99 escravosm (os dois ultimos sao
-    // do tipo "processamento!")
-    private       int             numAtualServidores;  //Quantidade de servidores ja
-    // adicionados ao CS OBS: Em qualquer momento numAtualServidores e no
-    // maximo igual a numMaxServidores
-    private       int             numAtualFilas;
-    private       boolean         ehEscalonador;
+    private final Collection<Object> servidores = new HashSet<>();
 
-    //Adiciona CSs Homogeneos ( maquinas (servidores de processamento),
-    // conexao de rede simples ou
-    //Internet(servidores de comunicacao) ).
+    private final Collection<Object> filas = new HashSet<>();
+
+    /**
+     * Capacidade maxima de servidores que podem ser adicionados ao CS. Esse numero considera apenas
+     * servidores de processamento (mestre e escravos) ou seja, se esse numero for 100, o servidor
+     * tera 101 servidores, sendo: 1 servidor de comunicacao, 1 mestre e 99 escravosm (os dois
+     * ultimos sao do tipo "processamento!")
+     */
+    private final int numMaxServidores;
+
+    private final int idCS;
+
+    /**
+     * Quantidade de servidores ja adicionados ao CS OBS: Em qualquer momento numAtualServidores e
+     * no maximo igual a numMaxServidores.
+     */
+    private int numAtualServidores = 0;
+
+    /**
+     * Na inicializacao do CS nao ha adicao de servidores. Inicialmente, nao ha servidor, entao nao
+     * ha servidor livre
+     */
+    private int numAtualFilas = 0;
+
+    private boolean ehEscalonador;
+
+    /**
+     * Adiciona CSs Homogeneos ( maquinas (servidores de processamento), conexao de rede simples ou
+     * Internet(servidores de comunicacao) ).
+     */
     public CentrosDeServico (final int identCS, final int nMaxServ, final int[] vetEscravos) {
         this(identCS, nMaxServ);
         this.setVetorEscravos(vetEscravos);
@@ -31,25 +45,15 @@ public class CentrosDeServico {
     public CentrosDeServico (final int identCS, final int nMaxServ) {
         this.idCS = Math.max(identCS, 0);
         this.numMaxServidores = Math.max(nMaxServ, 0);
-        this.setNumAtualServidores(0);
-        //Na inicializacao do CS nao ha adicao de servidores.
-        //Inicialmente, nao ha servidor, entao nao ha servidor livre
-        this.numAtualFilas = 0;
-        this.servidores    = new HashSet<Object>();
-        this.filas         = new HashSet<Object>();
     }
 
     public void setVetorEscravos (final int[] vetEscravos) {
-        int[] vetorEscravos;
+        final int[] vetorEscravos;
         vetorEscravos = vetEscravos;
         System.out.printf("\nthis.vetorEscravos.length - " + vetorEscravos.length);
         for (int i = 0; i < vetorEscravos.length; i++) {
             System.out.printf("\nthis.vetorEscravos[" + i + "] - " + vetorEscravos[i]);
         }
-    }
-
-    private void setNumAtualServidores (final int nAtualServ) {
-        this.numAtualServidores = (nAtualServ >= 0) ? nAtualServ : 0;
     }
 
     public void adicionaServidorProcto (final boolean msOuEsc) {
@@ -64,14 +68,10 @@ public class CentrosDeServico {
         this.servidores.add(new Object());
 
         System.out.printf(
-                "|\tServidor ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
-                this.numAtualServidores, this.idCS
+            "|\tServidor ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
+            this.numAtualServidores, this.idCS
         );
-        this.alteraNumAtualServidores(1); //Soma 1 ao numero atual de servidores
-    }
-
-    private void alteraNumAtualServidores (final int aSomar) {
-        this.numAtualServidores += aSomar;
+        this.numAtualServidores += 1;
     }
 
     public int getIdCS () {
@@ -81,44 +81,39 @@ public class CentrosDeServico {
     public void adicionaServidorCom () {
 
         this.ehEscalonador = false;
-        if (!this.ehEscalonador) {
-            System.out.printf("o CS %d NAO eh escalonador\n", this.idCS);
-        } else {
-            System.out.printf("o CS %d eh escalonador\n", this.idCS);
-        }
+        System.out.printf("o CS %d NAO eh escalonador\n", this.idCS);
         this.servidores.add(new Object());
 
         System.out.printf(
-                "|\tServidor ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
-                this.numAtualServidores, this.idCS
+            "|\tServidor ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
+            this.numAtualServidores, this.idCS
         );
-        this.alteraNumAtualServidores(1); //Soma 1 ao numero atual de servidores
+        this.numAtualServidores += 1;
     }
 
     //+--------------------------------+
     public void adicionaServidoresClr () {
         this.ehEscalonador = true;
-        if (!this.ehEscalonador) {System.out.printf("o CS %d NAO eh escalonador\n", this.idCS);} else {
-            System.out.printf("o CS %d eh escalonador\n", this.idCS);
-        }
+        System.out.printf("o CS %d eh escalonador\n", this.idCS);
 
         this.servidores.add(new Object());
 
         System.out.printf(
-                "|\tServidor ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
-                this.numAtualServidores, this.idCS
+            "|\tServidor ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
+            this.numAtualServidores, this.idCS
         );
-        this.alteraNumAtualServidores(1); //Soma 1 ao numero atual de servidores
+        this.numAtualServidores += 1;
 
         //Adiciona os escravos
         while (this.numAtualServidores < this.numMaxServidores) {
             this.servidores.add(new Object());
 
             System.out.printf(
-                    "|\tServidor ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
-                    this.numAtualServidores, this.idCS
+                "|\tServidor ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
+                this.numAtualServidores, this.idCS
             );
-            this.alteraNumAtualServidores(1); //Soma 1 ao numero atual de servidores
+            //Soma 1 ao numero atual de servidores
+            this.numAtualServidores += 1;
         }
         //Apos adicionar todos os servidores de processamento, adiciono o
         // servidor de comunicacao
@@ -129,11 +124,10 @@ public class CentrosDeServico {
         // getNumAtualServidores()-1
         //Ou seja, o proximo numero inteiro e getNumAtualServidores()
         System.out.printf(
-                "|\tServidor DE COMUNICACAO ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
-                this.numAtualServidores, this.idCS
+            "|\tServidor DE COMUNICACAO ID: %2d adicionado ao Centro de Servico ID: %2d |\n",
+            this.numAtualServidores, this.idCS
         );
-        final Object serv = new Object();
-        this.servidores.add(serv);
+        this.servidores.add(new Object());
 
         //Nao precisa adicionar valor a  numAtualServidores, pois o servidor
         // e de comunicacao e essa
@@ -145,11 +139,10 @@ public class CentrosDeServico {
     public void adicionaFila () {
         this.filas.add(new Object());
         System.out.printf(
-                "|\tFila ID: %2d adicionada ao Centro de Servico ID: %2d     |\n", this.numAtualFilas, this.idCS);
-        this.alteraNumAtualFilas(1);
-    }
-
-    private void alteraNumAtualFilas (final int aSomar) {
-        this.numAtualFilas += aSomar;
+            "|\tFila ID: %2d adicionada ao Centro de Servico ID: %2d     |\n",
+            this.numAtualFilas,
+            this.idCS
+        );
+        this.numAtualFilas += 1;
     }
 }
