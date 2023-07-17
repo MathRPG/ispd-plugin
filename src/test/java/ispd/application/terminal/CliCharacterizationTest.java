79ca0182d7b2a84dbecdb367d09e5dcedefc8dd6
Math_RPG <maugusilva.santos@gmail.com>
2023-07-17 12:52:17 -0300

    + Add system.out replace and reinstatement.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/test/java/ispd/application/terminal/CliCharacterizationTest.java
+++ src/test/java/ispd/application/terminal/CliCharacterizationTest.java
@@ -3,11 +3,24 @@ package ispd.application.terminal;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CliCharacterizationTest {
 
     private static final String[] NO_ARGS = {};
+
+    private final PrintStream           standardOut  = System.out;
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void replaceSystemOut () {
+        System.setOut(new PrintStream(this.outputStream));
+    }
 
     @Test
     void emptyArgs () {
@@ -20,5 +33,10 @@ class CliCharacterizationTest {
         final var actual   = exception.getMessage();
 
         assertEquals(expected, actual, "Exception message doesn't match");
+    }
+
+    @AfterEach
+    void reinstateStandardSystemOut () {
+        System.setOut(this.standardOut);
     }
 }
