b253732086c315b376ecbd2d4f631ded2d15bdbe
Math_RPG <maugusilva.santos@gmail.com>
2023-08-22 14:35:32 -0300

    r remove method.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/gui/BundleManager.java
+++ src/main/java/ispd/gui/BundleManager.java
@@ -7,10 +7,6 @@ public enum BundleManager {
 
     private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("text.gui");
 
-    public static ResourceBundle getBundle () {
-        return BUNDLE;
-    }
-
     public static String getText (final String key) {
         return BUNDLE.getString(key);
     }
