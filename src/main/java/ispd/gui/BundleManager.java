25ff2c48ddbdbab77b991be18af0f144e119382d
Math_RPG <maugusilva.santos@gmail.com>
2023-08-22 13:07:46 -0300

    R move resource bundle.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/gui/BundleManager.java
+++ src/main/java/ispd/gui/BundleManager.java
@@ -5,7 +5,7 @@ import java.util.*;
 public enum BundleManager {
     ;
 
-    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("ispd.idioma.gui");
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("text.gui");
 
     public static ResourceBundle getBundle () {
         return BUNDLE;
