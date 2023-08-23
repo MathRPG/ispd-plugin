78d3f699ac8d01cecb3b1093a6fc617eef290fff
Math_RPG <maugusilva.santos@gmail.com>
2023-08-23 08:31:06 -0300

    R! some refactoring.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/loaders/GenericPolicyLoader.java
+++ src/main/java/ispd/policy/loaders/GenericPolicyLoader.java
@@ -1,30 +1,31 @@
 package ispd.policy.loaders;
 
-import ispd.arquivo.xml.ConfiguracaoISPD;
-import ispd.policy.Policy;
-import ispd.policy.PolicyLoader;
-import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import ispd.arquivo.xml.*;
+import ispd.policy.*;
+import java.lang.reflect.*;
+import java.net.*;
+import java.util.logging.*;
+import org.jetbrains.annotations.*;
 
 public abstract class GenericPolicyLoader <T extends Policy<?>> implements PolicyLoader<T> {
 
+    private static final URL[] CLASS_LOADER_URL = { getIspdDirectoryUrl() };
+
     private static final URLClassLoader CLASS_LOADER = makeClassLoader();
 
-    private static URLClassLoader makeClassLoader () {
+    private static @NotNull URL getIspdDirectoryUrl () {
         try {
-            return URLClassLoader.newInstance(
-                new URL[] { ConfiguracaoISPD.DIRETORIO_ISPD.toURI().toURL(), },
-                GenericPolicyLoader.class.getClassLoader()
-            );
-        } catch (final MalformedURLException ex) {
-            Logger.getLogger(GenericPolicyLoader.class.getName())
-                .log(Level.SEVERE, "Could not create the loader!", ex);
-            throw new ExceptionInInitializerError(ex);
+            return ConfiguracaoISPD.DIRETORIO_ISPD.toURI().toURL();
+        } catch (final MalformedURLException e) {
+            throw new ExceptionInInitializerError(e);
         }
+    }
+
+    private static URLClassLoader makeClassLoader () {
+        return URLClassLoader.newInstance(
+            CLASS_LOADER_URL,
+            GenericPolicyLoader.class.getClassLoader()
+        );
     }
 
     protected abstract String getClassPath ();
@@ -33,7 +34,7 @@ public abstract class GenericPolicyLoader <T extends Policy<?>> implements Polic
     public T loadPolicy (final String policyName) {
         final var clsName = this.getClassPath() + policyName;
         try {
-            final var cls = GenericPolicyLoader.CLASS_LOADER.loadClass(clsName);
+            final var cls = CLASS_LOADER.loadClass(clsName);
             return (T) cls.getConstructor().newInstance();
         } catch (final ClassNotFoundException | InvocationTargetException |
                        InstantiationException | IllegalAccessException |
