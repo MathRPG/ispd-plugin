7f7cf037f243c388bd4280604574679cd979d974
Math_RPG <maugusilva.santos@gmail.com>
2023-08-25 13:05:50 -0300

    d remove unuseds.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/managers/CloudSchedulingPolicyManager.java
+++ src/main/java/ispd/policy/managers/CloudSchedulingPolicyManager.java
@@ -1,8 +1,6 @@
 package ispd.policy.managers;
 
-import ispd.arquivo.xml.*;
 import ispd.policy.*;
-import java.io.*;
 import java.util.*;
 
 /**
@@ -12,14 +10,4 @@ public class CloudSchedulingPolicyManager implements PolicyManager {
 
     public static final List<String> NATIVE_POLICIES =
         List.of(PolicyManager.NO_POLICY, "RoundRobin");
-
-    private static final String CLOUD_DIR_PATH =
-        String.join(File.separator, "policies", "scheduling", "cloud");
-
-    private static final File CLOUD_DIRECTORY =
-        new File(ConfiguracaoISPD.DIRETORIO_ISPD, CLOUD_DIR_PATH);
-
-    protected String className () {
-        return "CloudSchedulingPolicyManager.class";
-    }
 }
