86818a9d590c378da7ebaa498968e6d4ce788168
Math_RPG <maugusilva.santos@gmail.com>
2023-08-25 08:13:46 -0300

    d Remove unused method.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/arquivo/interpretador/simgrid/InterpretadorSimGrid.java
+++ src/main/java/ispd/arquivo/interpretador/simgrid/InterpretadorSimGrid.java
@@ -1,56 +1,15 @@
 package ispd.arquivo.interpretador.simgrid;
 
-import java.io.*;
-import javax.swing.*;
 import org.w3c.dom.*;
 
 public class InterpretadorSimGrid {
 
-    private static String fname = null;
+    private static final String fname = null;
 
-    private Document modelo = null;
+    private final Document modelo = null;
 
     public static String getFileName () {
         return fname;
-    }
-
-    private void setFileName (final File f) {
-        fname = f.getName();
-    }
-
-    public void interpreta (final File file1, final File file2) {
-        try {
-            try {
-                final var application_file = new FileInputStream(file1);
-                final var plataform_file   = new FileInputStream(file2);
-                final var parser           = SimGrid.getInstance(application_file);
-                this.setFileName(file1);
-                SimGrid.ReInit(application_file);
-                SimGrid.modelo();
-                this.setFileName(file2);
-                SimGrid.ReInit(plataform_file);
-                SimGrid.modelo();
-                final var error = parser.resultadoParser();
-                if (!error) {
-                    this.modelo = parser.getModelo().finishDocument();
-                }
-                parser.reset();
-            } catch (final ParseException ex) {
-                JOptionPane.showMessageDialog(
-                    null,
-                    ex.getMessage(),
-                    "Error",
-                    JOptionPane.WARNING_MESSAGE
-                );
-            }
-        } catch (final IOException ex) {
-            JOptionPane.showMessageDialog(
-                null,
-                ex.getMessage(),
-                "Error",
-                JOptionPane.WARNING_MESSAGE
-            );
-        }
     }
 
     public Document getModelo () {
