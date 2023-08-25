4311bbfbfcb1e639287fa14c3ad8368af20151c5
Math_RPG <maugusilva.santos@gmail.com>
2023-08-25 13:02:48 -0300

    r inline method.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/PolicyManager.java
+++ src/main/java/ispd/policy/PolicyManager.java
@@ -1,17 +1,6 @@
 package ispd.policy;
 
-import java.util.*;
-
 public interface PolicyManager {
 
     String NO_POLICY = "---";
-
-    /**
-     * Lists all available allocation policies.
-     *
-     * @return {@code ArrayList} with all allocation policies' names
-     */
-    default ArrayList<String> listar () {
-        return new ArrayList<>();
-    }
 }
