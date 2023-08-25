56767ab89d2502c6dcdf0a05d1e5a75b6d6700a4
Math_RPG <maugusilva.santos@gmail.com>
2023-08-25 13:06:03 -0300

    d remove unuseds.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/managers/VmAllocationPolicyManager.java
+++ src/main/java/ispd/policy/managers/VmAllocationPolicyManager.java
@@ -1,8 +1,6 @@
 package ispd.policy.managers;
 
-import ispd.arquivo.xml.*;
 import ispd.policy.*;
-import java.io.*;
 import java.util.*;
 
 /**
@@ -20,14 +18,4 @@ public class VmAllocationPolicyManager implements PolicyManager {
         "FirstFitDecreasing",
         "Volume"
     );
-
-    private static final String VM_DIR_PATH =
-        String.join(File.separator, "policies", "allocation", "vm");
-
-    private static final File VM_DIRECTORY =
-        new File(ConfiguracaoISPD.DIRETORIO_ISPD, VM_DIR_PATH);
-
-    protected String className () {
-        return "VmAllocationPolicyManager.class";
-    }
 }
