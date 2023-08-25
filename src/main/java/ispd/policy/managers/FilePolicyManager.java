c06820acc46857ca835bd891262d79093fc82a45
Math_RPG <maugusilva.santos@gmail.com>
2023-08-25 13:02:12 -0300

    d remove unused class, method.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/managers/FilePolicyManager.java
+++ src/main/java/ispd/policy/managers/FilePolicyManager.java
@@ -1,7 +1,6 @@
 package ispd.policy.managers;
 
 import ispd.policy.*;
-import java.io.*;
 import java.util.*;
 
 public abstract class FilePolicyManager implements PolicyManager {
@@ -9,13 +8,6 @@ public abstract class FilePolicyManager implements PolicyManager {
     private final ArrayList<String> policies = new ArrayList<>();
 
     protected FilePolicyManager () {
-    }
-
-    public static void createDirectory (final File dir)
-        throws IOException {
-        if (!dir.mkdirs()) {
-            throw new IOException("Failed to create directory " + dir);
-        }
     }
 
     /**
