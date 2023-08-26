5d8b63506835080d5684c95afe51f0dc281787f9
Math_RPG <maugusilva.santos@gmail.com>
2023-08-26 13:28:00 -0300

    r remove unused field.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/PolicyLoader.java
+++ src/main/java/ispd/policy/PolicyLoader.java
@@ -7,20 +7,6 @@ public interface PolicyLoader <T extends Policy<?>> {
     String NO_POLICY = "---";
 
     /**
-     * Grid scheduling policies available by default.
-     */
-    List<String> NATIVE_GRID_POLICIES = List.of(
-        NO_POLICY,
-        "RoundRobin",
-        "Workqueue",
-        "WQR",
-        "DynamicFPLTF",
-        "HOSEP",
-        "OSEP",
-        "EHOSEP"
-    );
-
-    /**
      * Cloud scheduling policies available by default.
      */
     List<String> NATIVE_CLOUD_POLICIES = List.of(NO_POLICY, "RoundRobin");
