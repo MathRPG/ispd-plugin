604cec400a5ba89d6fb44e5feda1e8b24212b9b0
Math_RPG <maugusilva.santos@gmail.com>
2023-08-26 08:33:59 -0300

    r Override method.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/loaders/GridSchedulingPolicyLoader.java
+++ src/main/java/ispd/policy/loaders/GridSchedulingPolicyLoader.java
@@ -1,6 +1,6 @@
 package ispd.policy.loaders;
 
-import ispd.policy.scheduling.grid.GridSchedulingPolicy;
+import ispd.policy.scheduling.grid.*;
 
 public class GridSchedulingPolicyLoader extends GenericPolicyLoader<GridSchedulingPolicy> {
 
@@ -8,6 +8,11 @@ public class GridSchedulingPolicyLoader extends GenericPolicyLoader<GridScheduli
 
     @Override
     protected String getClassPath () {
-        return GridSchedulingPolicyLoader.CLASS_PATH;
+        return CLASS_PATH;
+    }
+
+    @Override
+    public GridSchedulingPolicy loadPolicy (final String policyName) {
+        return super.loadPolicy(policyName);
     }
 }
