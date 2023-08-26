21a47b9d5798228d0a33ff74178ab35e29e9df4e
Math_RPG <maugusilva.santos@gmail.com>
2023-08-26 13:19:20 -0300

    r Simplify.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/loaders/GenericPolicyLoader.java
+++ src/main/java/ispd/policy/loaders/GenericPolicyLoader.java
@@ -1,25 +1,11 @@
 package ispd.policy.loaders;
 
-import static java.util.stream.Collectors.*;
-
 import ispd.policy.*;
 import java.util.*;
 import java.util.function.*;
-import java.util.stream.*;
 import org.jetbrains.annotations.*;
 
 public abstract class GenericPolicyLoader <T extends Policy<?>> implements PolicyLoader<T> {
-
-    protected static @NotNull <T> Map<String, Supplier<T>> makePolicyMap (
-        final @NotNull Stream<? extends Map.Entry<String, Supplier<T>>> entryStream
-    ) {
-        return entryStream.collect(toMap(
-            Map.Entry::getKey,
-            Map.Entry::getValue,
-            (x, y) -> y,
-            LinkedHashMap::new
-        ));
-    }
 
     protected abstract @NotNull Map<String, Supplier<T>> getPolicies ();
 
--- src/main/java/ispd/policy/loaders/GridSchedulingPolicyLoader.java
+++ src/main/java/ispd/policy/loaders/GridSchedulingPolicyLoader.java
@@ -4,24 +4,21 @@ import ispd.policy.scheduling.grid.*;
 import ispd.policy.scheduling.grid.impl.*;
 import java.util.*;
 import java.util.function.*;
-import java.util.stream.*;
 import org.jetbrains.annotations.*;
 
 public class GridSchedulingPolicyLoader extends GenericPolicyLoader<GridSchedulingPolicy> {
 
-    private static final Map<String, Supplier<GridSchedulingPolicy>> POLICIES =
-        makePolicyMap(policyEntries());
+    private static final Map<String, Supplier<GridSchedulingPolicy>> POLICIES;
 
-    private static @NotNull Stream<Map.Entry<String, Supplier<GridSchedulingPolicy>>> policyEntries () {
-        return Stream.of(
-            Map.entry("RoundRobin", RoundRobin::new),
-            Map.entry("Workqueue", Workqueue::new),
-            Map.entry("WQR", WQR::new),
-            Map.entry("DynamicFPLTF", DynamicFPLTF::new),
-            Map.entry("HOSEP", HOSEP::new),
-            Map.entry("OSEP", OSEP::new),
-            Map.entry("EHOSEP", EHOSEP::new)
-        );
+    static {
+        POLICIES = new LinkedHashMap<>();
+        POLICIES.put("RoundRobin", RoundRobin::new);
+        POLICIES.put("Workqueue", Workqueue::new);
+        POLICIES.put("WQR", WQR::new);
+        POLICIES.put("DynamicFPLTF", DynamicFPLTF::new);
+        POLICIES.put("HOSEP", HOSEP::new);
+        POLICIES.put("OSEP", OSEP::new);
+        POLICIES.put("EHOSEP", EHOSEP::new);
     }
 
     public static Map<String, Supplier<GridSchedulingPolicy>> getNativePolicies () {
