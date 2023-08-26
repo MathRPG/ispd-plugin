a1aef4c0d372f5592efba9c86966aa8c294fc467
Math_RPG <maugusilva.santos@gmail.com>
2023-08-26 08:35:05 -0300

    a empty variable.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/loaders/GridSchedulingPolicyLoader.java
+++ src/main/java/ispd/policy/loaders/GridSchedulingPolicyLoader.java
@@ -1,6 +1,8 @@
 package ispd.policy.loaders;
 
 import ispd.policy.scheduling.grid.*;
+import java.util.*;
+import java.util.function.*;
 
 public class GridSchedulingPolicyLoader extends GenericPolicyLoader<GridSchedulingPolicy> {
 
@@ -13,6 +15,7 @@ public class GridSchedulingPolicyLoader extends GenericPolicyLoader<GridScheduli
 
     @Override
     public GridSchedulingPolicy loadPolicy (final String policyName) {
+        final var map = Map.<String, Supplier<GridSchedulingPolicy>>of();
         return super.loadPolicy(policyName);
     }
 }
