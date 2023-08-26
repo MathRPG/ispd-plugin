88daa13e100c6b6f96b608bc47b81af1839993de
Math_RPG <maugusilva.santos@gmail.com>
2023-08-26 14:46:59 -0300

    r extract constant.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/Loader.java
+++ src/main/java/ispd/policy/Loader.java
@@ -9,12 +9,14 @@ import org.jetbrains.annotations.*;
 
 public abstract class Loader <T extends Policy<?>> {
 
+    private static final String INFO_PATTERN = "Loading policy ''{0}''";
+
     private static final Logger LOGGER = Logger.getLogger(Loader.class.getName());
 
     protected abstract @NotNull Map<String, Supplier<T>> policyMap ();
 
     public T loadPolicy (final String name) {
-        LOGGER.info(() -> MessageFormat.format("Loading policy ''{0}''", name));
+        LOGGER.info(() -> MessageFormat.format(INFO_PATTERN, name));
 
         return Optional.of(name)
             .map(this::getSupplierByName)
