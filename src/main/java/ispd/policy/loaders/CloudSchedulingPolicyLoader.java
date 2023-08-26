6e1fbd88ab24fadadac2253ebfbc08c3c3f0350d
Math_RPG <maugusilva.santos@gmail.com>
2023-08-26 08:53:28 -0300

    A use supplier map in cloud.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/loaders/CloudSchedulingPolicyLoader.java
+++ src/main/java/ispd/policy/loaders/CloudSchedulingPolicyLoader.java
@@ -1,13 +1,28 @@
 package ispd.policy.loaders;
 
-import ispd.policy.scheduling.cloud.CloudSchedulingPolicy;
+import ispd.policy.scheduling.cloud.*;
+import ispd.policy.scheduling.cloud.impl.*;
+import java.util.*;
+import java.util.function.*;
 
 public class CloudSchedulingPolicyLoader extends GenericPolicyLoader<CloudSchedulingPolicy> {
+
+    private static final Map<String, Supplier<CloudSchedulingPolicy>> SUPPLIER_MAP = Map.of(
+        "RoundRobin", RoundRobin::new
+    );
 
     private static final String CLASS_PATH = "ispd.policy.scheduling.cloud.impl.";
 
     @Override
     protected String getClassPath () {
-        return CloudSchedulingPolicyLoader.CLASS_PATH;
+        return CLASS_PATH;
+    }
+
+    @Override
+    public CloudSchedulingPolicy loadPolicy (final String policyName) {
+        return Optional.of(policyName)
+            .map(SUPPLIER_MAP::get)
+            .map(Supplier::get)
+            .orElseThrow(() -> new UnknownPolicyException(policyName));
     }
 }
