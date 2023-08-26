ccba8a235e717e358cf81cf179fc0c836baf7dd9
Math_RPG <maugusilva.santos@gmail.com>
2023-08-26 08:55:26 -0300

    A use supplier map in vm.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/loaders/VmAllocationPolicyLoader.java
+++ src/main/java/ispd/policy/loaders/VmAllocationPolicyLoader.java
@@ -1,13 +1,31 @@
 package ispd.policy.loaders;
 
-import ispd.policy.allocation.vm.VmAllocationPolicy;
+import ispd.policy.allocation.vm.*;
+import ispd.policy.allocation.vm.impl.*;
+import java.util.*;
+import java.util.function.*;
 
 public class VmAllocationPolicyLoader extends GenericPolicyLoader<VmAllocationPolicy> {
+
+    private static final Map<String, Supplier<VmAllocationPolicy>> SUPPLIER_MAP = Map.of(
+        "RoundRobin", RoundRobin::new,
+        "FirstFit", FirstFit::new,
+        "FirstFitDecreasing", FirstFitDecreasing::new,
+        "Volume", Volume::new
+    );
 
     private static final String CLASS_PATH = "ispd.policy.allocation.vm.impl.";
 
     @Override
     protected String getClassPath () {
-        return VmAllocationPolicyLoader.CLASS_PATH;
+        return CLASS_PATH;
+    }
+
+    @Override
+    public VmAllocationPolicy loadPolicy (final String policyName) {
+        return Optional.of(policyName)
+            .map(SUPPLIER_MAP::get)
+            .map(Supplier::get)
+            .orElseThrow(() -> new UnknownPolicyException(policyName));
     }
 }
