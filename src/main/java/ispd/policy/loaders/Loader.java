17017035a99c1175cd26a6980742c05b3c792a0b
Math_RPG <maugusilva.santos@gmail.com>
2023-08-26 13:52:02 -0300

    F add logging.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/loaders/Loader.java
+++ src/main/java/ispd/policy/loaders/Loader.java
@@ -1,19 +1,25 @@
 package ispd.policy.loaders;
 
 import ispd.policy.*;
+import java.text.*;
 import java.util.*;
 import java.util.function.*;
+import java.util.logging.*;
 import org.jetbrains.annotations.*;
 
 public abstract class Loader <T extends Policy<?>> {
 
+    private static final Logger LOGGER = Logger.getLogger(Loader.class.getName());
+
     protected abstract @NotNull Map<String, Supplier<T>> policyMap ();
 
-    public T loadPolicy (final String policyName) {
-        return Optional.of(policyName)
+    public T loadPolicy (final String name) {
+        LOGGER.info(() -> MessageFormat.format("Loading policy ''{0}''", name));
+
+        return Optional.of(name)
             .map(this::getSupplierByName)
             .map(Supplier::get)
-            .orElseThrow(() -> new UnknownPolicyException(policyName));
+            .orElseThrow(() -> new UnknownPolicyException(name));
     }
 
     private Supplier<T> getSupplierByName (final String name) {
