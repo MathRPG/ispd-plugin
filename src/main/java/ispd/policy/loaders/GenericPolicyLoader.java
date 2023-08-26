9988515db3812ba5f66fcf826457feb6781e7b2a
Math_RPG <maugusilva.santos@gmail.com>
2023-08-26 09:00:24 -0300

    r remove unused fields.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/policy/loaders/GenericPolicyLoader.java
+++ src/main/java/ispd/policy/loaders/GenericPolicyLoader.java
@@ -1,29 +1,11 @@
 package ispd.policy.loaders;
 
-import ispd.arquivo.xml.*;
 import ispd.policy.*;
-import java.net.*;
 import java.util.*;
 import java.util.function.*;
 import org.jetbrains.annotations.*;
 
 public abstract class GenericPolicyLoader <T extends Policy<?>> implements PolicyLoader<T> {
-
-    private static final ClassLoader CLASS_LOADER = GenericPolicyLoader.class.getClassLoader();
-
-    private static final URLClassLoader URL_CLASS_LOADER = makeClassLoader();
-
-    private static URLClassLoader makeClassLoader () {
-        return URLClassLoader.newInstance(new URL[] { getIspdDirectoryUrl() }, CLASS_LOADER);
-    }
-
-    private static @NotNull URL getIspdDirectoryUrl () {
-        try {
-            return ConfiguracaoISPD.DIRETORIO_ISPD.toURI().toURL();
-        } catch (final MalformedURLException e) {
-            throw new ExceptionInInitializerError(e);
-        }
-    }
 
     @Override
     public T loadPolicy (final String policyName) {
