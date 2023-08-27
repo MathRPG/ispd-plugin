090036fe9371facfd9fd63ad33986baf089e694e
Math_RPG <maugusilva.santos@gmail.com>
2023-08-27 17:11:43 -0300

    F remove sysout.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/motor/faults/Hardware.java
+++ src/main/java/ispd/motor/faults/Hardware.java
@@ -2,7 +2,6 @@ package ispd.motor.faults;
 
 import ispd.gui.*;
 import ispd.motor.queues.*;
-import ispd.motor.queues.centers.impl.*;
 import ispd.motor.simul.*;
 import java.util.*;
 import javax.swing.*;
@@ -24,37 +23,12 @@ public class Hardware {
 
         final var vms = gridQueueNetwork.getVMs();
 
-        System.out.println("---------------------------------------");
 
         if (vms == null) {
-            System.out.println("Rede de filas é nula na classe " + "CloudSequential.java");
             return;
         }
 
         final var machines = gridQueueNetwork.getMaquinasCloud();
         final int id       = new Random().nextInt(machines.size());
-
-        System.out.printf("""
-                          Rede de filas das VMs não é nula na classe CloudSequential.java
-                          Listagem da rede de filas:
-                          Rede de Filas: %s
-                          Rede de Filas get VMs: %s
-                          Há máquinas alocadas no redeDeFilas
-                          Rede de Filas Cloud get PMs: %s
-                          Rede de Filas Cloud: getMaquinasCloud: %s
-                          Quantidade de Máquinas alocadas ao mestre: %d
-                          Número da posição da maquina sorteada: %d
-                          Máquina sorteada desligada: %d
-                          Máquina que o status é igual a 2: %d
-                          """,
-                          gridQueueNetwork,
-                          vms,
-                          vms,
-                          machines,
-                          machines.size(),
-                          id,
-                          id,
-                          CloudMachine.DESLIGADO
-        );
     }
 }
