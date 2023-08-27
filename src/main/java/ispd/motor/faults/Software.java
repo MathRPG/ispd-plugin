83d533827c6a5810ff09e513ea4055dfe9e640d4
Math_RPG <maugusilva.santos@gmail.com>
2023-08-27 16:55:31 -0300

    C remove clutter.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/motor/faults/Software.java
+++ src/main/java/ispd/motor/faults/Software.java
@@ -2,9 +2,7 @@ package ispd.motor.faults;
 
 import ispd.gui.*;
 import ispd.motor.queues.*;
-import ispd.motor.queues.centers.impl.*;
 import ispd.motor.simul.*;
-import java.util.*;
 import javax.swing.*;
 
 public class Software {
@@ -19,18 +17,9 @@ public class Software {
             return;
         }
 
-        //sorteio de máquina alocada ao mestre  Falha de omissão de hardware:
-        final Random random = new Random();
-        int          draw   = random.nextInt(redeDeFilas.getMaquinasCloud().size());
-        //tornar a posição sorteada ==Desligada
-        draw = CloudMachine.DESLIGADO;
-
         final int NovoRedeDeFilas = redeDeFilas.getMaquinasCloud().size() - 1;
 
-        //escreva o vetor redeDeFilas com a posição [draw} com status == DESLIGADO
         for (int i = 0; i <= NovoRedeDeFilas; i++) {
         }
-        //Refazer o escalonamento: Técnica: Regate do Workflow??? ou Redistribuição das tarefas??
-
     }
 }
