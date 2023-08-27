7b6072b3bbc39261f780bc779d30646efe6f5a54
Math_RPG <maugusilva.santos@gmail.com>
2023-08-27 17:12:44 -0300

    r rename param.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/motor/faults/Hardware.java
+++ src/main/java/ispd/motor/faults/Hardware.java
@@ -6,10 +6,10 @@ import javax.swing.*;
 
 public class Hardware {
 
-    public static void showMessage (final ProgressTracker janela) {
+    public static void showMessage (final ProgressTracker progressTracker) {
         new PickSimulationFaultsDialog();
         JOptionPane.showMessageDialog(null, "Hardware Failure selected.");
-        janela.println("Hardware failure created.");
-        janela.print(" -> ");
+        progressTracker.println("Hardware failure created.");
+        progressTracker.print(" -> ");
     }
 }
