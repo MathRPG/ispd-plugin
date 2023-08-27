9c69014113147b4ca30cca55e8122b2315629083
Math_RPG <maugusilva.santos@gmail.com>
2023-08-27 16:54:52 -0300

    F remove sysouts.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/motor/faults/Software.java
+++ src/main/java/ispd/motor/faults/Software.java
@@ -19,37 +19,16 @@ public class Software {
             return;
         }
 
-        System.out.println("---------------------------------------");
-        System.out.println("Rede de filas das VMs não é nula na classe CloudSequential.java");
-        System.out.println("Listagem da rede de filas: ");
-        System.out.println("Rede de Filas: " + redeDeFilas);
-        System.out.println("Rede de Filas get VMs: " + redeDeFilas.getVMs());
-        System.out.println("Há máquinas alocadas no redeDeFilas");
-        System.out.println("Rede de Filas Cloud get VMs: " + redeDeFilas.getVMs());
-        System.out.println("Rede de Filas Cloud: getMaquinasCloud: "
-                           + redeDeFilas.getMaquinasCloud());
-        System.out.println("Quantidade de Máquinas alocadas ao mestre: " + redeDeFilas
-            .getMaquinasCloud()
-            .size());
         //sorteio de máquina alocada ao mestre  Falha de omissão de hardware:
         final Random random = new Random();
         int          draw   = random.nextInt(redeDeFilas.getMaquinasCloud().size());
-        System.out.println("Número da posição da maquina sorteada: " + draw); //Exemplo: [27]
-        System.out.println("Máquina sorteada desligada: " + draw);
         //tornar a posição sorteada ==Desligada
         draw = CloudMachine.DESLIGADO;
-        System.out.println(
-            "Status da máquina desligada => public static final int DESLIGADO = 2: " + draw);
 
         final int NovoRedeDeFilas = redeDeFilas.getMaquinasCloud().size() - 1;
-        System.out.println("Novo redeDeFilas: " + NovoRedeDeFilas);
 
         //escreva o vetor redeDeFilas com a posição [draw} com status == DESLIGADO
         for (int i = 0; i <= NovoRedeDeFilas; i++) {
-            System.out.println("Novo Rede de Filas: " + NovoRedeDeFilas);
-            System.out.println("Novo Rede de Filas Cloud get VMs: " + redeDeFilas.getVMs());
-            System.out.println("Novo Rede de Filas Cloud: getMaquinasCloud: "
-                               + redeDeFilas.getMaquinasCloud());
         }
         //Refazer o escalonamento: Técnica: Regate do Workflow??? ou Redistribuição das tarefas??
 
