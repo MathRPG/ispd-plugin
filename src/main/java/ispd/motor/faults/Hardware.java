bb23f74c92f86e90ae527b1f08ea30c419c13381
Math_RPG <maugusilva.santos@gmail.com>
2023-08-27 17:12:01 -0300

    r remove clutter.
    Signed-off-by: Math_RPG <maugusilva.santos@gmail.com>

--- src/main/java/ispd/motor/faults/Hardware.java
+++ src/main/java/ispd/motor/faults/Hardware.java
@@ -3,32 +3,17 @@ package ispd.motor.faults;
 import ispd.gui.*;
 import ispd.motor.queues.*;
 import ispd.motor.simul.*;
-import java.util.*;
 import javax.swing.*;
 
 public class Hardware {
-
-    private static void selectFaults (final ProgressTracker janela) {
-        new PickSimulationFaultsDialog();
-        JOptionPane.showMessageDialog(null, "Hardware Failure selected.");
-        janela.println("Hardware failure created.");
-        janela.print(" -> ");
-    }
 
     public void FIHardware1 (
         final ProgressTracker janela,
         final CloudQueueNetwork gridQueueNetwork
     ) {
-        selectFaults(janela);
-
-        final var vms = gridQueueNetwork.getVMs();
-
-
-        if (vms == null) {
-            return;
-        }
-
-        final var machines = gridQueueNetwork.getMaquinasCloud();
-        final int id       = new Random().nextInt(machines.size());
+        new PickSimulationFaultsDialog();
+        JOptionPane.showMessageDialog(null, "Hardware Failure selected.");
+        janela.println("Hardware failure created.");
+        janela.print(" -> ");
     }
 }
